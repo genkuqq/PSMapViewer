2332aba19acc73150c0aedecf66a9dce7938fbc3
genku <55231188+genkuqq@users.noreply.github.com>
2024-05-08 09:12:35 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = (process.env.NODE_ENV = "production");
 const nextConfig = {
-  basePath: isProd ? "/PsychonautStationMapViewer" : "",
-  output: "export",
-  distDir: "dist",
+  basePath: "/PsychonautStationMapViewer",
 };
 
 export default nextConfig;
