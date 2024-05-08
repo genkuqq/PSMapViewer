9b42e47a311149953736493bb51301c6d08292f4
genku <koraygench@gmail.com>
2024-05-08 08:42:13 +0300

    pages test

--- README.md
+++
@@ -1,9 +0,0 @@
-# Psychonaut Station Map Viewer
-
-This project made it with nextjs & leaflet.
-
-Thanks to AA07 for idea.
-
-Created for [Psychonaut Station](https://github.com/psychonaut-station/PsychonautStation)
-
-You feel free to use.
--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const isProd = (process.env.NODE_ENV = "production");
-const nextConfig = {
-  basePath: isProd ? "/PsychonautStationMapViewer" : "",
-  output: "export",
-  distDir: "dist",
-};
-
-export default nextConfig;
